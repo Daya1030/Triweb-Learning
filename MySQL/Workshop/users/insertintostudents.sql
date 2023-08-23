6303859e7cb15152626365c68f33ef31ec6338c1
Daya1030 <dayaramthapa51@gmail.com>
2023-08-23 15:35:00 +0545

    values inserted on students table

---
+++ MySQL/Workshop/users/insertintostudents.sql
@@ -0,0 +1,4 @@
+insert into students(id,students_name,number, class) 
+values(1,'daya','9876', 5),(2,'Ram','9875', 5),(3,'Hari','9874', 6),(4,'shyam','9879', 6),
+(5,'geeta','1111', 7),(6,'sita','2222', 7),(7,'Anjali','3333', 1),(8,'Amana','4444', 2),
+(9,'dabang','9999', 3),(10,'Nilesh','7777', 4),(11,'Aakriti','6666', 4)
