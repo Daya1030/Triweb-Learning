25f2ebb0b9e4788711330f10dd327fa20cbacb0d
Daya1030 <dayaramthapa51@gmail.com>
2023-08-24 14:40:32 +0545

    class added

---
+++ ES6/class.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(sName, classId) {
+      this.sName = sName;
+      this.classId = classId;
+    }
+  }
+  
+  const std1 = new Student("Daya", "9th");
+  const std2 = new Student("Ram", "7th");
+  
+  console.log(std1);
+  console.log(std2);
