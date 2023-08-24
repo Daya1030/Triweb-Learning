41a2a168fbb656170020c100493853423241669c
Daya1030 <dayaramthapa51@gmail.com>
2023-08-24 11:45:09 +0545

    Constant inside value changed

--- ES6/constants.js
+++ ES6/constants.js
@@ -1,7 +1,27 @@
 // constants 
-var a = 5;
-console.log(a);
+//var a = 5;
+//console.log(a);
 //a = 7
 //console.log(a);
+
+// Example2
+// const a = 5;
+// console.log(a);
+//a=7 // TypeError: Assignment to constant variable.
+//console.log(a);
+
+
+
+//Example2
+const obj = {name:"daya", age:23};
+console.log("Before", obj);
+// Is the below line will work???
+//obj = {name:"Ram", age:23};// TypeError: Assignment to constant variable.
+//console.log("After", obj);
+
+//Then how to change the content?
+obj.name = "Ram";
+obj.age = 24;
+console.log("After", obj);
 
 
