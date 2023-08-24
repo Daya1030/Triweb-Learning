13785297b4bd0f168cf226a59ef1ae70672fde68
Daya1030 <dayaramthapa51@gmail.com>
2023-08-24 12:28:52 +0545

    scoping added

---
+++ ES6/scoping.js
@@ -0,0 +1,16 @@
+//scoping
+   // Block-scoped variables
+   //Example 1
+   //console.log(a); //RefrenceError: cannot access 'a' before initialization
+  // let a = 8; 
+
+  //Example 2
+ // {
+   // var n = 5;
+   // x = 7;
+   // const y = 9;
+  // }
+   //console.log(n);
+   //console.log(x); //RefrenceError: x is not defined
+   //console.loh(y); // ReferenceError: y is not defined
+    // Block-scoped Functions
