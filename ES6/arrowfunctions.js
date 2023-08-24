9123bfcde7a6a3317dc03111a50bff3c26c5e206
Daya1030 <dayaramthapa51@gmail.com>
2023-08-24 12:34:50 +0545

    traditional array function added

--- ES6/arrowfunctions.js
+++ ES6/arrowfunctions.js
@@ -1,5 +1,13 @@
-const xyz = () => {
-    console.log("xyz", this);
-}
-console.log("here", this);
-xyz();
+//const xyz = () => {
+ //   console.log("xyz", this);
+//}
+//console.log("here", this);
+//xyz();
+
+
+//2 expression bodies
+let arr = [1,2,3,4];
+const arrmap = arr.map(function(element){
+    return element * 2 ;
+})
+console.log(arrmap);
