9fe30fff7451f2d199d529064f3024fcf5d02254
Daya1030 <dayaramthapa51@gmail.com>
2023-08-24 13:49:25 +0545

    templeteliterals practiced

---
+++ ES6/templateliterals.js
@@ -0,0 +1,11 @@
+// template literals
+
+function printData(name, age) {
+    let toPrint = `Student's name is ${name}
+  and age is ${age}`;
+    console.log(toPrint);
+  }
+  
+  let sName = "Daya";
+  let sAge = 23;
+  printData(sName, sAge);
