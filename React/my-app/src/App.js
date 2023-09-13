ee6b2048e03640be6f11ad128ac13070a6325ec7
Daya1030 <dayaramthapa51@gmail.com>
2023-09-13 12:39:55 +0545

    app file updated

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,25 +1,32 @@
-import FirstComponent from './components/FirstComponent'
-import Backdrop from './components/Backdrop'
-import { useState } from "react";
+
+import './App.css';
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product';
+import AddProduct from './pages/AddProduct'
+
+import FavouriteItems from './pages/FavouriteItems';
+
+import {Routes, Route } from 'react-router-dom';
+import Layout from './components/layouts/Layout';
 
 function App() {
-  const [modalopen, setModalOpen] = useState(false);
-
-  const modalcloseHandler = ()=>{
-    setModalOpen(false);
-  }
-
-  const openModalHandler = ()=> {
-    setModalOpen(true);
-  }
-
   return (
-   <div>
-      <h1>Its my application</h1>
-      {modalopen?<FirstComponent owner="Daya" reason="For Demo" closeModal={modalcloseHandler}/>:null}
-      {modalopen?<Backdrop />:null}
-      <button onClick={openModalHandler}>Open Modal</button>
-    </div> 
+    <Layout >
+      
+      <Routes>
+          <Route path='/' element={<Home/>}></Route> 
+          <Route path='/about' element={<About/>}></Route> 
+          <Route path='/contact' element={<Contact/>}></Route> 
+          <Route path='/product' element={<Product/>}></Route> 
+          <Route path='/product/add' element={<AddProduct/>}></Route> 
+          <Route path='/product/favourite' element={<FavouriteItems/>}></Route> 
+      </Routes>
+      
+       
+    </Layout>
   );
 }
 
