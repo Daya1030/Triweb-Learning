f5185c585cd35b2cbdf6f84a4c368ed7ed5e15e2
Daya1030 <dayaramthapa51@gmail.com>
2023-09-13 12:37:55 +0545

    main menu added

---
+++ React/my-app/src/components/layouts/MainMenu.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import MainMenuStyle from './MainMenu.module.css'
+import FavouriteContext from "../../store/ContextFavourite";
+import { useContext } from "react";
+
+function MainMenu() {
+    const favouriteContext = useContext(FavouriteContext);
+    const totalFavouriteItem = favouriteContext.favouriteItems.length;
+    return (
+        <header className={MainMenuStyle.header}>
+            <nav >
+                <ul >
+                    <li ><Link to="/">Home</Link></li>
+                    <li > <Link to="/about">About</Link></li>
+                    <li ><Link to="/contact">Contact</Link></li>
+                    <li ><Link to="/product">Product</Link></li>
+                    <li ><Link to="/product/add">Add Product</Link></li>
+                    <li className={MainMenuStyle.favouriteCount} ><Link to="/product/favourite">{totalFavouriteItem}</Link></li>
+                </ul>
+            </nav>
+
+            
+           
+            
+        </header>
+    )
+}
+export default MainMenu;
