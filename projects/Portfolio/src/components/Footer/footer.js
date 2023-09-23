6cbf21eab795ff0c76808c6eec391657300a5342
Daya1030 <dayaramthapa51@gmail.com>
2023-09-23 13:03:32 +0545

    footer added

---
+++ projects/Portfolio/src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css';
+
+function footer() {
+  return (
+    <footer className="footer">
+      Copyright &#169; 2023 Dayaram Thapa. All right received.
+    </footer>
+  );
+}
+
+export default footer;
