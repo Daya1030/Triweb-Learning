117d946621e33ded24fd387ba30c67d9a3b2cf7b
Daya1030 <dayaramthapa51@gmail.com>
2023-09-27 16:02:23 +0545

    contact and intro file updated

--- projects/Portfolio/src/components/Contact/contact.js
+++ projects/Portfolio/src/components/Contact/contact.js
@@ -1,40 +1,55 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 import './contact.css';
+import emailjs from '@emailjs/browser'; 
 import FacebookIcon from '../../assets/facebook-icon.png';
 import TwiterIcon from '../../assets/twitter.png';
 import YouteubeIcon from '../../assets/youtube.png';
 import InstagramIcon from '../../assets/instagram.png';
-import emailjs from '@emailjs/browser';
 
 function Contact() {
   const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_9o1xh47', 'template_w79crba', form.current, 'zttHXl5rSwlcH0CjZDsQ2')
-      .then((result) => {
+    emailjs
+      .sendForm('service_9o1xh47', 'template_w79crba', form.current, 'zttHXl5rSwlcH0CjZDsQ2')
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
+
   return (
     <section id="contactPage">
-        <h1 className="contactPageTitle">Contact Me</h1>
-        <span className="contactDesc">Please fill out the form below to discuss any work with me.</span>
-        <form className="contractForm" ref={form} onsubmit={sendEmail}>
-            <input type="text" className="name" placeholder='Your Name' name='your_name'/>
-            <input type="text" className="email" placeholder='Your Email' name='your_email'/>
-            <textarea className='msg' name="message" row="5" placeholder='Your Message'></textarea>
-            <button type='submit' value='send' className="submitBtn">Submit</button>
-            <div className="links">
-                <img src={FacebookIcon} alt="" className="link" />
-                <img src={TwiterIcon} alt="" className="link" />
-                <img src={YouteubeIcon} alt="" className="link" />
-                <img src={InstagramIcon} alt="" className="link" />
-            </div>
-        </form>
-
+      <h1 className="contactPageTitle">Contact Me</h1>
+      <span className="contactDesc">Please fill out the form below to discuss any work with me.</span>
+      <form className="contractForm" ref={form} onSubmit={sendEmail}>
+        <input type="text" className="name" placeholder="Your Name" name="your_name" />
+        <input type="text" className="email" placeholder="Your Email" name="your_email" />
+        <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
+        <button type="submit" value="send" className="submitBtn">
+          Submit
+        </button>
+        <div className="links">
+          <a href="https://www.facebook.com/dayaram.thapa.18/" target="_blank" rel="noopener noreferrer" className="link">
+            <img src={FacebookIcon} alt="Facebook" className="link" />
+          </a>
+          <a href="https://twitter.com/Dayaram1030" target="_blank" rel="noopener noreferrer" className="link">
+            <img src={TwiterIcon} alt="Twitter" className="link" />
+          </a>
+          <a href="https://www.youtube.com/channel/UCc0Go3Sp9awQt5cGnlYnsXA" target="_blank" rel="noopener noreferrer" className="link">
+            <img src={YouteubeIcon} alt="YouTube" className="link" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="link">
+            <img src={InstagramIcon} alt="Instagram" className="link" />
+          </a>
+        </div>
+      </form>
     </section>
   );
 }
--- projects/Portfolio/src/components/Intro/intro.js
+++ projects/Portfolio/src/components/Intro/intro.js
@@ -9,8 +9,9 @@ function intro() {
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello</span>
-        <span className="introText">I'm<span className="introName">Daya</span> <br/>Full Stack Developer on Mernstack</span>
-        <p className="introPara">I am a skilled full stack developer with experience in Mernstack.</p>
+        <span className="introText">I'm <span className="introName">DayaRam Thapa</span> <br/>Full Stack Developer on Mernstack</span>
+        <p className="introPara">I am a skilled full stack developer with experience in Mernstack.I have used Rest Api to develop backend of <br/>
+        QuizManager.I also know HTML, CSS, JavaScript, Typescript, Python, NodeJS,MongoDB,MySQL,Git etc.</p>
         <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg' />Hire Me</button></Link>
       </div>
       <img src={bg} alt="profile" className="bg" />
