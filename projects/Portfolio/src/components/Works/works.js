411bb92f107f7dfafdc872429070f2f852a06d14
Daya1030 <dayaramthapa51@gmail.com>
2023-09-27 16:02:56 +0545

    works file updated

--- projects/Portfolio/src/components/Works/works.js
+++ projects/Portfolio/src/components/Works/works.js
@@ -7,22 +7,30 @@ import Portfolio4 from '../../assets/portfolio-4.png';
 import Portfolio5 from '../../assets/portfolio-5.png';
 import Portfolio6 from '../../assets/portfolio-6.png';
 
-const Works=() => {
+const Works = () => {
+  const portfolioItems = [
+    { image: Portfolio1, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' }, 
+    { image: Portfolio2, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' }, 
+    { image: Portfolio3, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' }, 
+    { image: Portfolio4, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' }, 
+    { image: Portfolio5, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' }, 
+    { image: Portfolio6, link: 'https://github.com/Daya1030/Triweb-Learning/tree/main/projects/QuizManager' },
+  ];
+
   return (
     <section id="works">
-        <h2 className="worksTitle">My Portfolio</h2>
-        <span className="worksDesc">i am a full stack developer</span>
-        <div className="worksImgs">
-            <img src={Portfolio1} alt="" className="workImg" />
-            <img src={Portfolio2} alt="" className="workImg" />
-            <img src={Portfolio3} alt="" className="workImg" />
-            <img src={Portfolio4} alt="" className="workImg" />
-            <img src={Portfolio5} alt="" className="workImg" />
-            <img src={Portfolio6} alt="" className="workImg" />
-        </div>
-        <button className="workBtn">See More</button>
+      <h2 className="worksTitle">My Portfolio</h2>
+      <span className="worksDesc">I am a full-stack developer and here are my some Works</span>
+      <div className="worksImgs">
+        {portfolioItems.map((item, index) => (
+          <a key={index} href={item.link} target="_blank" rel="noopener noreferrer">
+            <img src={item.image} alt={`Portfolio ${index + 1}`} className="workImg" />
+          </a>
+        ))}
+      </div>
+      
     </section>
   );
-}
+};
 
 export default Works;
