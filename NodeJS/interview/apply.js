87d84caf0e5233b0c5d5825332bcf2080c8ea4d0
Daya1030 <dayaramthapa51@gmail.com>
2023-10-10 17:19:49 +0545

    apply function practiced

---
+++ NodeJS/interview/apply.js
@@ -0,0 +1,29 @@
+/*
+In call() we pass arguments
+
+In apply() we pass array
+*/
+
+function marksDetails(...marks){
+    this.subject1 = marks[0];
+    this.subject2 = marks[1];
+    this.subject3 = marks[2];
+    this.subject4 = marks[3];
+    this.subject5 = marks[4];
+
+
+    let status = 'pass';
+    marks.forEach((num)=>{
+        if(num<33){
+            status = 'fail';
+        }
+    })
+    this.result = status;
+}
+
+function student(sname, ...marks){
+    this.sname = sname;
+    marksDetails.apply(this, marks)
+}
+const st1 = new student("Daya", 44,34,36,79,98);
+console.log(st1);
