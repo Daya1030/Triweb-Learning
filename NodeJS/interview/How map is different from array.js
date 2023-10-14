6a9f890fedb74b42e8f225a9943096f4ade21f52
Daya1030 <dayaramthapa51@gmail.com>
2023-10-14 15:58:46 +0545

    map function diff added

---
+++ NodeJS/interview/How map is different from array.js
@@ -0,0 +1,15 @@
+/*
+-> An array is a collection of items stored at contiguous memory locations
+
+*/
+
+let arr = [1,2,3,4,5,6,7,8,9];
+
+/*
+-> A map is an associative container that stores elements in a mapped manner
+-> Use to store records
+*/
+let mapE = {
+    uname: "Daya",
+    mobile: 981337
+}
